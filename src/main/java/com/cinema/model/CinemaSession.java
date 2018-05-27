de830ab091e70afb728cd251a727402165bfe91a
alexnar <sasha_nar@mail.ru>
2018-05-27 20:22:41 +0300

    h2 ability to watch data

---
+++ src/main/java/com/cinema/model/CinemaSession.java
@@ -0,0 +1,21 @@
+package com.cinema.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "CinemaSession")
+public class CinemaSession {
+  @Id
+  @Column(name = "Id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(name = "StartTime")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date startTime;
+
+  @ManyToOne
+  @JoinColumn(name = "MovieId")
+  private Movie movie;
+}
